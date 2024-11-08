9d34fb2fc75424ac314e493607e39d61071c72cd
net0821 <c612201320@tokushima-u.ac.jp>
2024-11-08 16:07:02 +0900

    Update linetrace.ino

--- robot_dev_3/robot_dev_3/Integrate/linetrace.ino
+++ robot_dev_3/robot_dev_3/Integrate/linetrace.ino
@@ -16,7 +16,9 @@ int distance() {
 
   return dist;
 }
+
 float sum_e = 0;
+
 float turnTo(float theta_r) {
   float u;
   float KP = 4.0;
@@ -36,13 +38,16 @@ float turnTo(float theta_r) {
   if ( u<-180 ) u = -180; // 飽和
   return u;
 }
+
 void movement()
 {
   float diff;
   dist = distance(); 
+  move_color = identifyColor(red_G, green_G, blue_G);
   static unsigned long startTime; // static変数，時間計測ははunsigned long
   // この変数は1秒停止の開始時間を覚えておくために使用
-  switch (mode_G) 
+  if(color_b == true){
+    switch (mode_G) 
   {
     case 0:
       startTime = timeNow_G;//start時間を保存
@@ -50,7 +55,7 @@ void movement()
       break;  // break文を忘れない（忘れるとその下も実行される）
 
     case 1:
-      motors.setSpeeds(100, 100);//100の速度でロボットを動かす
+      motors.setSpeeds(200, 200);//100の速度でロボットを動かす
       if(maintainState(2000))//nマイクロ秒経ったらモードを2にする
         mode_G = 2;
       break;
@@ -65,7 +70,7 @@ void movement()
     break;
    case 3:
     if(dist >= 5){//物体との距離が5cm以内になるまで前進
-      motors.setSpeeds(100,100);   
+      motors.setSpeeds(150,150);   
     }  else if(0 < dist && dist < 5){//物体との距離が5cm以下なら静止
       motors.setSpeeds(0, 0);
       mode_G =4;         
@@ -75,17 +80,59 @@ void movement()
       }
     }
     break;
-    case 4:
-      
+    case 4: 
       diff = turnTo(0);
       motorL_G = diff;
       motorR_G = -diff;
       motors.setSpeeds(motorL_G, motorR_G);
       if(abs(0-heading_G)<= 5){
+        mode_G = 5;
+      }
+    break;
+    case 5:
+      motors.setSpeeds(150, 150);
+      if(move_color == 2 || move_color == 3){
         mode_G = 0;
       }
+  } 
   }
-  timePrev_G=timeNow_G;
+
+  switch(move_color){
+    case 0:
+      break;
+    case 1:
+      color_b = false;
+      motors.setSpeeds(-100, -100);
+      delay(1000);
+      motors.setSpeeds(150, -150);
+      delay(1400);
+      color_b = true;
+      break;
+    case 2:
+      color_b = false;
+      motors.setSpeeds(100, 100);
+      delay(500);
+      motors.setSpeeds(-150, -150);
+      delay(1000);
+      motors.setSpeeds(150, -150);
+      delay(1400);
+      color_b = true;
+      break;
+    case 3:
+      color_b = false;
+      motors.setSpeeds(100, 100);
+      delay(500);
+      motors.setSpeeds(-150, -150);
+      delay(1000);
+      motors.setSpeeds(150, -150);
+      delay(1400);
+      color_b = true;
+      break;
+    case 4:
+      break;
+  }
+  
+  
 }
 
 void info_write(){
@@ -104,8 +151,18 @@ void info_write(){
   Serial.print("green:");
   Serial.print(green_G);//RGBのgreen(0~255)の値をシリアルモニタに出力
   Serial.print(",");
+  Serial.print("move_color:");//0 白　1　黒　2　青　3　赤　4　不明
+  Serial.print(move_color);//RGBのgreen(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("heading_p:");//0 白　1　黒　2　青　3　赤　4　不明
+  Serial.print(heading_p);//RGBのgreen(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("Head:");//0 白　1　黒　2　青　3　赤　4　不明
+  Serial.print(Head);//RGBのgreen(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
   Serial.print("blue:");
   Serial.println(blue_G);//RGBのblue(0~255)の値をシリアルモニタに出力
+  
 }
 
 int maintainState( unsigned long period )
@@ -126,17 +183,15 @@ int maintainState( unsigned long period )
 }
 
 int identifyColor(int red, int green, int blue) {
-  if (red > red && green > green && blue > blues) {
+  if (red > 250 && green > 250 && blue > 250) {
     return 0;//白
-  } else if (red < reds && green < greens && blue < blues) {
+  } else if (red < 30 && green < 45 && blue < 45) {
     return 1; //黒
-  } else if (red < reds && green < greens && blue > blues) {
+  } else if (red < 55&& green < 95  && blue > 110) {
     return 2; //青
-  } else if (red > reds && green < greens && blue < blues) {
+  } else if (red > 130 && green < 65  && blue < 65 ) {
     return 3; //赤
   } else  {
-    return 4"白";
-  } else {
-    return "不明";
-  }
+    return 4;//不明
+  } 
 }
