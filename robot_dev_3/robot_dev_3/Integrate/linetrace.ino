5a7a157774deb23bad3fea998fd3b9b7a359aeaf
net0821 <c612201320@tokushima-u.ac.jp>
2024-11-22 13:01:59 +0900

    Update linetrace.ino

--- robot_dev_3/robot_dev_3/Integrate/linetrace.ino
+++ robot_dev_3/robot_dev_3/Integrate/linetrace.ino
@@ -1,153 +1,251 @@
-#include <Wire.h>
-#include <ZumoMotors.h>
-#include <Pushbutton.h>
-#include <LSM303.h>
+int distance() {
+  unsigned long interval;        //Echoのパルス幅(μs)
+  int dist;                      //距離(cm)
+  
+  digitalWrite(trig, HIGH);      //10μsのパルスを超音波センサのTrigピンに出力
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
 
-ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON);
-LSM303 compass;
+  interval = pulseIn(echo, HIGH, 23071);               //Echo信号がHIGHである時間(μs)を計測
+  dist = (0.61 * 25 + 331.5) * interval / 10000 / 2;   //距離(cm)に変換
 
-const int trig = 2;              //TrigピンをArduinoの2番ピンに
-const int echo = 4;              //EchoピンをArduinoの4番ピンに
-float red_G, green_G, blue_G; // カラーセンサで読み取ったRGB値（0-255）
-bool isRed_G, isBlue_G;
-int mode_G; // タスクのモードを表す状態変数
-unsigned long timeInit_G, timeNow_G, timePrev_G; //  スタート時間，経過時間, 1回前
-int motorR_G, motorL_G; // 左右のZumoのモータに与える回転力
-int dist;
-float mx=0, my=0, mz=0;
-float ax=0, ay=0, az=0;
-float  heading_G= 0;
-float heading_p = 0;
-bool color_b;
-bool change_black;
-bool mp;
-int move_color;
-int Head=0;
-int straight_time = 0;
+  delay(60);                     //trigがHIGHになる間隔を60ms以上空ける（超音波センサの仕様）
 
-void setup()
-{
-  button.waitForButton(); //ユーザボタンが押されるまで待機
-  Serial.begin(9600);
-  pinMode(trig, OUTPUT); //trig を出力ポートに設定
-  pinMode(echo, INPUT); //echo を入力ポートに設定
-  Wire.begin();
-  setupCompass();
-  button.waitForButton(); 
-  CalibrationColorSensor();
-  calibrationCompass();
-  button.waitForButton();
-  timeInit_G = millis();
-  timePrev_G=0;
-  mode_G = 0;
-  motorR_G = 0;
-  motorL_G = 0;
-  color_b = true;
-  change_black = false;
-  mp = false;
-  move_color = 0;
+  if(dist >=150)
+    dist = 150;
+
+  return dist;
 }
 
-void loop()
-{
-  getRGB(red_G, green_G, blue_G); // カラーセンサでRGB値を取得(0-255)
-  timeNow_G = millis() - timeInit_G; // 経過時間
-  //motors.setSpeeds(motorL_G, motorR_G); // 左右モーターへの回転力入力
-  if (timeNow_G-timePrev_G<100) {
-    return;
+float sum_e = 0;
+
+float turnTo(float theta_r) {
+  float u;
+  float KP = 4.0;
+  float TIinv = 2/1000.0;
+  heading_G = atan2(my,mx) * 180 / M_PI;
+  if (heading_G<0) heading_G += 360;
+  float e = theta_r-heading_G;
+  if (e<-180) e+=360;
+  if (e>180)  e-=360;
+  if (abs(e) > 45.0 ) { // |e|>45のときはP制御
+    u = KP*e;           // P制御
+  } else {              // |e|<=45 の時はPI制御
+    sum_e += TIinv*e*(timeNow_G-timePrev_G);
+    u = KP*(e+sum_e);   // PI 制御
   }
-  compass.read();
-  compass.m_min.x = min(compass.m.x,compass.m_min.x);  compass.m_max.x = max(compass.m.x,compass.m_max.x);
-  compass.m_min.y = min(compass.m.y,compass.m_min.y);  compass.m_max.y = max(compass.m.y,compass.m_max.y);
-  compass.m_min.z = min(compass.m.z,compass.m_min.z);  compass.m_max.z = max(compass.m.z,compass.m_max.z);
-  ax = compass.a.x/256; //map(compass.a.x,-32768,32767,-128,127);
-  ay = compass.a.y/256; //map(compass.a.y,-32768,32767,-128,127);
-  az = compass.a.z/256; //map(compass.a.z,-32768,32767,-128,127);
-  mx = map(compass.m.x,compass.m_min.x,compass.m_max.x,-128,127);
-  my = map(compass.m.y,compass.m_min.y,compass.m_max.y,-128,127);
-  mz = map(compass.m.z,compass.m_min.z,compass.m_max.z,-128,127); 
-  
-  heading_p = atan2(my,mx) * 180 / M_PI;
-  Head=checkHead(heading_p);
-  
-  sendData(); // データ送信
-
-  movement();
-
-  info_write();
+  if ( u> 180 ) u = 180;  // 飽和
+  if ( u<-180 ) u = -180; // 飽和
+  return u;
 }
 
-int checkHead(float G){
-  int hougaku;
-  
-  if(G<0){
-    G+=360;
-  }
-  if(337.5<G&&G<=360||0<=G && G<=22.5){
-    hougaku=1;//北
-  }
-  else if(22.5<G&&G<=67.5)
-  {
-    hougaku=2;//北東
-  }
-  else if(67.5<G&&G<=112.5)
-  {
-    hougaku=3;//東
-  }
-   else if(112.5<G&&G<=157.5)
-  {
-    hougaku=4;//南東
-  }
-   else if(157.5<G&&G<=202.5)
-  {
-    hougaku=5;//南
-  }
-  else if(202.5<G&&G<=247.5)
-  {
-    hougaku=6;//南西
-  }
-   else if(247.5<G&&G<=292.5)
-  {
-    hougaku=7;//西
-  }
-   else if(292.5<G&&G<=337.5)
-  {
-    hougaku=8;//北西
-  }
-  return hougaku;
+void linetrace_P()
+{
+  static float lightMin = 50; // 各自で設定
+  static float lightMax = 150; // 各自で設定
+  static float speed = 80; // パラメーター
+  static float Kp = 1.3; // パラメーター
+  float lightNow;
+  float speedDiff;
+
+  lightNow = (red_G + green_G + blue_G) / 3.0;
+  if (lightNow < (lightMin + lightMax) / 2.0) // 右回転
+    speedDiff = -Kp * map(speed, 0, 100, lightMin,(lightMin + lightMax) / 2.0);
+  else if(lightNow > (lightMin + lightMax) / 2.0) // 左回転
+    speedDiff = Kp * map(speed, 0, 100, lightMax,(lightMin + lightMax) / 2.0);
+  else 
+    speedDiff = 0;
+
+  motorL_G = speed - speedDiff;//反時計回り
+  motorR_G = speed + speedDiff;
+
+  //motorL_G = speed + speedDiff;//時計回り
+  //motorR_G = speed - speedDiff;
 }
 
-// 通信方式2
-void sendData()
+void movement()
 {
-  static unsigned long timePrev = 0;
-  static boolean flag_start = true; // 最初の通信かどうか
-  int inByte; 
+  float diff;
+  dist = distance(); 
+  move_color = identifyColor(red_G, green_G, blue_G);
+  static unsigned long startTime; // static変数，時間計測ははunsigned long
+  // この変数は1秒停止の開始時間を覚えておくために使用
+  if(color_b == true){
+    switch (mode_G) 
+  {
+    case 0:
+      startTime = timeNow_G;//start時間を保存
+      
+      mode_G = 1;//モードを1に変更する
+      break;  // break文を忘れない（忘れるとその下も実行される）
 
-  // if文の条件： 最初の通信である || 
-  // 最後のデータ送信から500ms経過 || 
-  // (データ送信要求が来ている && 最後のデータ送信から50ms経過)
-  if (flag_start == true || 
-      timeNow_G - timePrev > 500 || 
-      (Serial.available() > 0 && timeNow_G - timePrev > 50)) 
- {
-    flag_start = false;
-    while(Serial.available() > 0)
-    { 
-      // 送信要求が複数来ていた場合は全て読み込む
-      inByte = Serial.read();
+    case 1:
+      change_black = false;
+      motors.setSpeeds(150, 150);//100の速度でロボットを動かす
+      if(maintainState(2000))//nマイクロ秒経ったらモードを2にする
+      {
+        if(mp == false){
+          mode_G = 2;
+        }else if(mp == true){
+         mp = false;
+        }
+      }
+        
+      break;
+      
+   case 2:
+      motors.setSpeeds(160, -160);//150の速度で回転する
+      if(0 < dist && dist < 15){//物体との距離が15cm以内になったらモードを3にする
+        mode_G = 3; 
+      }
+      if(maintainState(2500))
+        mode_G = 1;//nマイクロ秒経ったらモードを2にする
+    break;
+   case 3:
+    if(dist >= 5){//物体との距離が5cm以内になるまで前進
+      motors.setSpeeds(150,150);
+      straight_time = timeNow_G; 
+    }else if(timeNow_G - straight_time > 5000 && dist >= 5){
+      mode_G = 1;
+    }else if(0 < dist && dist < 5){//物体との距離が5cm以下なら静止
+      motors.setSpeeds(0, 0);
+      mode_G =4;         
+      delay(1000);
     }
-    
-    Serial.write('H');
-    Serial.write(mode_G);
-    Serial.write(dist);
-    Serial.write(Head);
-    
-    //Serial.write((int)red_G);
-    //Serial.write((int)green_G );
-    //Serial.write((int)blue_G);
+    break;
+    case 4: 
+      diff = turnTo(0);
+      motorL_G = diff*0.7;
+      motorR_G = -diff*0.7;
+      motors.setSpeeds(motorL_G, motorR_G);
+      if(abs(0-heading_G)<= 5){
+        mode_G = 5;
+      }
+    break;
+    case 5:
+      change_black = true;
+      motors.setSpeeds(100, 100);
+      if(move_color == 2){
+        mode_G = 1;
+      }
 
-    timePrev = timeNow_G;
+      if(move_color == 3){
+        mode_G = 1;
+      }
+
+      if(move_color == 1){
+        mode_G = 6;
+      }
+    break;
+    case 6:
+    linetrace_P();
+    motors.setSpeeds(motorL_G, motorR_G);
+    if(move_color == 2){
+      mode_G = 1;
+    }
+  } 
   }
+
+  switch(move_color){
+    case 0:
+      break;
+    case 1:
+      if(change_black == false){
+        color_b = false;
+        motors.setSpeeds(-100, -100);
+        delay(1000);
+        motors.setSpeeds(150, -150);
+        delay(1400);
+        color_b = true;
+        mp = true;
+      }
+      break;
+    case 2:
+      color_b = false;
+      motors.setSpeeds(100, 100);
+      delay(500);
+      motors.setSpeeds(-150, -150);
+      delay(1000);
+      motors.setSpeeds(150, -150);
+      delay(1400);
+      color_b = true;
+      mp = true;
+      break;
+    case 3:
+      color_b = false;
+      motors.setSpeeds(100, 100);
+      delay(500);
+      motors.setSpeeds(-150, -150);
+      delay(1000);
+      motors.setSpeeds(150, -150);
+      delay(1400);
+      color_b = true;
+      mp = true;
+      break;
+    case 4:
+      break;
+  }
+  
+  
+}
+
+void info_write(){
+  Serial.print("timeNow:");
+  Serial.print(timeNow_G);
+  Serial.print(",");
+  Serial.print("mode:");
+  Serial.print(mode_G);//モードをシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("dist:");
+  Serial.print(dist); //距離(cm)をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("red:");
+  Serial.print(red_G);//RGBのred(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("green:");
+  Serial.print(green_G);//RGBのgreen(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("move_color:");//0 白　1　黒　2　青　3　赤　4　不明
+  Serial.print(move_color);//RGBのgreen(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("heading_p:");//0 白　1　黒　2　青　3　赤　4　不明
+  Serial.print(heading_p);//RGBのgreen(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("Head:");//0 白　1　黒　2　青　3　赤　4　不明
+  Serial.print(Head);//RGBのgreen(0~255)の値をシリアルモニタに出力
+  Serial.print(",");
+  Serial.print("blue:");
+  Serial.println(blue_G);//RGBのblue(0~255)の値をシリアルモニタに出力
+  
+}
+
+int maintainState( unsigned long period )
+{
+  static int flagStart = 0; // 0: 待ち状態，1: 現在計測中
+  static unsigned long startTime = 0;
+  if ( flagStart == 0 ) { // 待ち状態の場合は計測開始
+    startTime = timeNow_G; // 計測を開始した timeNow_G の値を覚えておく
+    flagStart = 1; // 現在計測中にしておく
+  }
+  if ( timeNow_G - startTime > period ) { // 経過時間が指定時間を越えた
+    flagStart = 0; // 待ち状態に戻しておく
+    startTime = 0; // なくても良いが，形式的に初期化
+    return 1;
+  }
+  else
+  return 0;
+}
+
+int identifyColor(int red, int green, int blue) {
+  if (red > 250 && green > 250 && blue > 250) {
+    return 0;//白
+  } else if (red < 30 && green < 45 && blue < 45) {
+    return 1; //黒
+  } else if (red < 55&& green < 95  && blue > 110) {
+    return 2; //青
+  } else if (red > 130 && green < 65  && blue < 65 ) {
+    return 3; //赤
+  } else  {
+    return 4;//不明
+  } 
 }
